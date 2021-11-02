d959f2b086a3b24e8b46aea40e6b730f5f5bb305
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-11-02 16:02:15 -0500

    ch 6

--- gameraterapi/views/auth.py
+++ gameraterapi/views/auth.py
@@ -5,7 +5,7 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.permissions import AllowAny
 from rest_framework.response import Response
 
-from raterapi.models import Rater
+from gameraterapi.models import Rater
 
 @api_view(['POST'])
 @permission_classes([AllowAny])
