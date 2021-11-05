65ced89a51017600b5af2a4794babc10d5c99e12
Alexander Schroyer <alexanderschroyer@gmail.com>
2021-11-05 15:03:55 -0500

    gameview added

--- raterapp/urls.py
+++ raterapp/urls.py
@@ -15,10 +15,16 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.conf.urls import include
+from rest_framework import routers
 from django.urls import path
 from gameraterapi.views import register_user, login_user
+from gameraterapi.views import GameView
+
+router = routers.DefaultRouter(trailing_slash=False)
+router.register(r'games', GameView, 'game')
 
 urlpatterns = [
+    path('', include(router.urls)),
     path('register', register_user),
     path('login', login_user),
     path('api-auth', include('rest_framework.urls', namespace='rest_framework')),
